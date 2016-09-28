6b7a2f7b6b7389cd0fe2fd8ea8c29e0b12a1cff3
flyaogogo <flyao_good@163.com>
2016-09-28 23:39:28 +0800

    修改图片存储在磁盘上

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # voosky
 this is test project !
+
+配置：
+1、在server.xml中配置两个目录，一个是项目工程目录，一个是图片存储虚拟路径 ，如：
+      <Host>
+      	... ...
+      	
+		<Context path="/vooskycms" docBase="E:\EGit\gitvoosky\voosky\WebRoot" reloadable="true" />
+		
+		 <!-- 设置图片虚拟路径[访问时路径为/static_img] -->  
+     	<Context path="/static_img" docBase="D:\vooskyimage" reloadable="false" />
+      </Host>
+
+2、在config.properties中设置服务器上的磁盘路径，此路径要对应server.xml中设置的图片虚拟路径
+
+upload.service.disk=d://vooskyimage
--- src/com/tmwrk/voosky/module/util/CalcUtil.java
+++ src/com/tmwrk/voosky/module/util/CalcUtil.java
@@ -1,7 +1,12 @@
 package com.tmwrk.voosky.module.util;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Properties;
 
 import javax.servlet.ServletContext;
@@ -29,7 +34,7 @@ public class CalcUtil {
 	
 
 	/**
-	 * 上传文件
+	 * 上传文件  服务器所在的  本地磁盘
 	 * @param isUpdate 是否已经上传
 	 * @param fbase 上传必备三个参数类
 	 * @param fileDirectory 上传到服务器的文件目录
@@ -60,6 +65,8 @@ public class CalcUtil {
 		
 		String retStatus = null ;
 		UploadFileBase fb = new UploadFileBase() ;
+		// 上传到服务器上的路径
+		String serviceFilePathStr = null ;
 		try {
 			//正则式过滤非指定格式
 			String specifiedFormat = "image/jpeg,image/gif,image/png,image/bmp,image/jpg" ;
@@ -82,6 +89,134 @@ public class CalcUtil {
 			*/
 			ServletContext context = request.getSession().getServletContext();
 			destPath = context.getRealPath("/") + slideFilePathStr ;
+			///******************************////
+			//System.out.println(" before :" + destPath);
+			/////******************************////
+			
+			File destFile = new File(destPath, myFileFileName);
+			if (destFile.exists()) {
+				if(isUpdate){
+					imageFilePath = imageFilePath.replace("\\", "/") ;
+					fb.setStatus(FILE_UPDATE_STATUS_SUCCESS);
+					fb.setFileRealPath(imageFilePath);
+					return fb;
+				}else{
+					destFile.delete() ;
+				}
+				
+			}else{
+				boolean exiFlag = true ;
+				if(!new File(destPath).exists()){
+					exiFlag = new File(destPath).mkdirs() ;
+				}
+				if(!exiFlag){
+					retStatus = "创建目录失败！或请手动创建upload/" + fileDirectory + "目录！" ;
+					fb.setStatus(FILE_UPDATE_STATUS_ERROR);
+					fb.setMessage(retStatus);
+					return fb;
+				}
+			}
+			destFile.createNewFile() ;
+			
+			//上传文件
+			FileUtils.copyFile(fbase.getImageFile(), destFile);
+			
+			
+			imageFilePath = imageFilePath.replace("\\", "/") ;
+			
+			//*************图片存储到服务器上的磁盘上*****************//
+			//http://blog.sina.com.cn/s/blog_8d960c4c0101cd4m.html
+			serviceFilePathStr = request.getScheme() + "://" + request.getServerName() + ":" 
+									+ request.getServerPort() + request.getContextPath() + "/" + imageFilePath  ;
+
+//			System.out.println( " after :" + serviceFilePathStr);
+//			System.out.println(fbase.getImageFile() + " --->>>" + destFile);
+//			System.out.println( " imageFilePath :" + imageFilePath);
+			String encode = judyCharacterEncoding(serviceFilePathStr); 
+			URL theURL = new URL(serviceFilePathStr);
+			String virtualServerImagePath = readConfigProperties().getProperty("upload.service.disk") ;
+			//服务器上的文件下载到服务器本地磁盘
+			downloadFile(theURL,myFileFileName, virtualServerImagePath + slideFilePathStr,encode);
+			
+			/////******************************//// 
+			
+			fb.setStatus(FILE_UPDATE_STATUS_SUCCESS);
+			fb.setFileRealPath(imageFilePath);
+			retStatus = "图片上传成功！" ;
+			log.info(retStatus);
+			return fb;
+		} catch (IOException e) {
+			e.printStackTrace();
+			retStatus = "图片上传失败！" ;
+			fb.setStatus(FILE_UPDATE_STATUS_ERROR);
+			fb.setMessage(retStatus);
+			log.error(retStatus + " : " + e);
+			return fb;
+		}finally{
+			//删除服务器上的文件
+			if(serviceFilePathStr != null){
+				File serviceFilePath = new File(serviceFilePathStr) ;
+				serviceFilePath.delete() ;
+			}
+		}
+		
+	}
+	/**
+	 * 上传文件 到服务器  原始处理方法
+	 * @param isUpdate 是否已经上传
+	 * @param fbase 上传必备三个参数类
+	 * @param fileDirectory 上传到服务器的文件目录
+	 * @param request http请求
+	 * @return
+	 */
+	public static UploadFileBase dealUploadServiceInfo(boolean isUpdate , UploadFileBase fbase , String fileDirectory , HttpServletRequest request){
+		
+		if(fbase.getImageFile()==null){
+			return null ;
+		}
+		
+		/*
+		String os = System.getProperty("os.name") ;
+		String driver = "" ;
+		System.out.println("os 操作系统类型：" + os);
+		if(os.toLowerCase().startsWith("win")){
+			driver = readConfigProperties().getProperty("upload.drive.path") ;
+		}else if(os.toLowerCase().indexOf("linux")>=0){
+			driver = "/" ;
+		}
+		 */
+		String myFileFileName = "image_" + fbase.getImageFileFileName() ;
+		
+		//String slideFilePathStr = driver + "vooskyupload/" + fileDirectory + "/" ;
+		String slideFilePathStr = "upload/" + fileDirectory + "/" ;
+		
+		String imageFilePath = slideFilePathStr + myFileFileName;
+		
+		String retStatus = null ;
+		UploadFileBase fb = new UploadFileBase() ;
+		try {
+			//正则式过滤非指定格式
+			String specifiedFormat = "image/jpeg,image/gif,image/png,image/bmp,image/jpg" ;
+			if(!specifiedFormat.contains(fbase.getImageFileContentType())){
+				retStatus = "请上传指定范围内的图片（jpeg,gif,png,bmp,jpg）!" ;
+				fb.setStatus(FILE_UPDATE_STATUS_ERROR);
+				fb.setMessage(retStatus);
+				return fb ;
+			}
+			
+			// 创建文件
+			String destPath = null ;
+			/*
+			if("".equals(driver)){
+				ServletContext context = request.getSession().getServletContext();
+				destPath = context.getRealPath("/") + slideFilePathStr ;
+			}else{
+				destPath = slideFilePathStr ;
+			}
+			 */
+			ServletContext context = request.getSession().getServletContext();
+			destPath = context.getRealPath("/") + slideFilePathStr ;
+		
 			
 			File destFile = new File(destPath, myFileFileName);
 			if (destFile.exists()) {
@@ -175,5 +310,67 @@ public class CalcUtil {
 			e.printStackTrace();
 		}
 		return props ;
+	}
+	
+	public static void downloadFile(URL theURL,String fileName, String filePath,String encode)
+			throws IOException {
+		
+		File dirFile = new File(filePath);
+		if (!dirFile.exists()) {// 文件路径不存在时，自动创建目录
+			dirFile.mkdir();
+		}
+		// 从服务器上获取图片并保存
+		URLConnection connection = theURL.openConnection();
+		connection.setRequestProperty("contentType", encode);
+		connection.setConnectTimeout(5 * 1000);  
+        
+		InputStream in = connection.getInputStream();
+		FileOutputStream os = new FileOutputStream(filePath + "/" + fileName);
+		byte[] buffer = new byte[4 * 1024];
+		int read;
+		while ((read = in.read(buffer)) > 0) {
+			os.write(buffer, 0, read);
+		}
+		os.close();
+		in.close();
+	}
+	
+	/**
+	 * 字符串编码格式判断及转码
+	 * @param character
+	 * @return
+	 * @throws UnsupportedEncodingException
+	 */
+	private static String judyCharacterEncoding(String character) throws UnsupportedEncodingException{
+		String temp = null ;
+		String enCode = null ;
+		if(character.equals(new String(character.getBytes("ISO-8859-1"), "ISO-8859-1"))) {
+          temp = new String(character.getBytes("ISO-8859-1"),"UTF-8");
+          enCode = "ISO-8859-1" ;
+        }else if(character.equals(new String(character.getBytes("GBK"), "GBK"))) {
+            temp = new String(character.getBytes("GBK"),"UTF-8");
+            enCode = "GBK" ;
+        }else if(character.equals(new String(character.getBytes("UTF-8"), "UTF-8"))) {
+        	temp = new String(character.getBytes("UTF-8"),"UTF-8");
+        	enCode = "UTF-8" ;
+        }else if(character.equals(new String(character.getBytes("GB2312"), "GB2312"))) {
+        	temp = new String(character.getBytes("GB2312"),"UTF-8");
+        	enCode = "GB2312" ;
+        }else if(character.equals(new String(character.getBytes("US-ASCII"), "US-ASCII"))) {
+        	temp = new String(character.getBytes("US-ASCII"),"UTF-8");
+        	enCode = "US-ASCII" ;
+        }else if(character.equals(new String(character.getBytes("UTF-16BE"), "UTF-16BE"))) {
+        	temp = new String(character.getBytes("UTF-16BE"),"UTF-8");
+        	enCode = "UTF-16BE" ;
+        }else if(character.equals(new String(character.getBytes("UTF-16LE"), "UTF-16LE"))) {
+        	temp = new String(character.getBytes("UTF-16LE"),"UTF-8");
+        	enCode = "UTF-16LE" ;
+        }else if(character.equals(new String(character.getBytes("UTF-16"), "UTF-16"))) {
+        	temp = new String(character.getBytes("UTF-16"),"UTF-8");
+        	enCode = "UTF-16" ;
+        }
+		System.out.println("---------character---->> :" + temp);
+		System.out.println("---------enCode---->> :" + enCode);
+		return enCode ;
 	}
 }
