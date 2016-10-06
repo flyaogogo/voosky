d484f44c8c3b4fd6a614964de9f5269d1df949f9
flyaogogo <flyao_good@163.com>
2016-10-06 22:47:09 +0800

    上传带空格的Url文件

--- src/com/tmwrk/voosky/module/util/CalcUtil.java
+++ src/com/tmwrk/voosky/module/util/CalcUtil.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Properties;
 
@@ -14,8 +15,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
-import sun.net.www.protocol.http.HttpURLConnection;
 
 import com.tmwrk.voosky.database.vo.UploadFileBase;
 
@@ -58,6 +57,8 @@ public class CalcUtil {
 		}
 		*/
 		String myFileFileName = "image_" + fbase.getImageFileFileName() ;
+		// 处理上传文件带空格
+		myFileFileName = myFileFileName.replace(" ", "") ;
 		
 		//String slideFilePathStr = driver + "vooskyupload/" + fileDirectory + "/" ;
 		String slideFilePathStr = "upload/" + fileDirectory + "/" ;
@@ -156,12 +157,139 @@ public class CalcUtil {
 		}finally{
 			//删除服务器上的文件
 			if(serviceFilePathStr != null){
+				System.out.println(serviceFilePathStr);
 				File serviceFilePath = new File(serviceFilePathStr) ;
 				serviceFilePath.delete() ;
 			}
 		}
 		
 	}
+	
+	
+	/**
+	 * 处理拼接的字符串
+	 * @param parentId
+	 * @return
+	 */
+	public static String[] dealParentId(String parentId){
+		String[] arr = new String[2] ;
+		
+		String navAlise = null ;
+		//处理  父  子  级的 拼装
+		if (parentId == null || "".equals(parentId) || "0".equals(parentId)) {
+			parentId = "0";
+			navAlise = "";
+		} else {
+			String[] pgs = parentId.split("@");
+
+			parentId = pgs[0].trim();
+			parentId = parentId.replaceFirst("^0*", "");
+			if (pgs.length == 1) {
+				navAlise = "-";
+			} else {
+				String level = pgs[1];
+				if ("".equals(level)) {
+					navAlise = "";
+				} else {
+					navAlise = level + "-";
+				}
+			}
+		}
+		arr[0] = parentId ;
+		arr[1] = navAlise ;
+		return arr ;
+	}
+	
+	/**
+	 * wfluo 2015.07.23 add
+	 * 读取系统config.properties配置文件
+	 * @return
+	 */
+	public static Properties readConfigProperties() {
+		Properties props = new Properties();
+		try {
+			props.load(CalcUtil.class.getClassLoader().getResourceAsStream("config.properties"));
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		return props ;
+	}
+	
+	public static void downloadFile(URL theURL,String fileName, String filePath,String encode)
+			throws IOException {
+		
+		File dirFile = new File(filePath);
+		if (!dirFile.exists()) {// 文件路径不存在时，自动创建目录
+			dirFile.mkdir();
+		}
+		// 从服务器上获取图片并保存
+		HttpURLConnection connection = (HttpURLConnection)theURL.openConnection();
+		//System.out.println("get Encode : " + connection.getContentEncoding());
+		connection.setRequestProperty("User-Agent","Mozilla/5.0 ( compatible ) ");
+		connection.setRequestProperty("Accept","*/*");
+		connection.setRequestProperty("contentType", encode);
+		connection.setConnectTimeout(5 * 1000);  
+		connection.setRequestProperty("Accept-Language", "zh-CN");  
+		connection.setDoOutput(true);
+        connection.setDoInput(true);
+        connection.setInstanceFollowRedirects(false); 
+        connection.setRequestMethod("POST"); 
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded"); 
+        connection.setRequestProperty("charset", "utf-8");
+        connection.setUseCaches (false);
+
+		
+		
+		InputStream in = connection.getInputStream();
+		FileOutputStream os = new FileOutputStream(filePath + "/" + fileName);
+		byte[] buffer = new byte[4 * 1024];
+		int read;
+		while ((read = in.read(buffer)) > 0) {
+			os.write(buffer, 0, read);
+		}
+		os.close();
+		in.close();
+	}
+	
+	/**
+	 * 字符串编码格式判断及转码
+	 * @param character
+	 * @return
+	 * @throws UnsupportedEncodingException
+	 */
+	private static String judyCharacterEncoding(String character) throws UnsupportedEncodingException{
+		//String temp = null ;
+		String enCode = null ;
+		if(character.equals(new String(character.getBytes("ISO-8859-1"), "ISO-8859-1"))) {
+          //temp = new String(character.getBytes("ISO-8859-1"),"UTF-8");
+          enCode = "ISO-8859-1" ;
+        }else if(character.equals(new String(character.getBytes("GBK"), "GBK"))) {
+            //temp = new String(character.getBytes("GBK"),"UTF-8");
+            enCode = "GBK" ;
+        }else if(character.equals(new String(character.getBytes("UTF-8"), "UTF-8"))) {
+        	//temp = new String(character.getBytes("UTF-8"),"UTF-8");
+        	enCode = "UTF-8" ;
+        }else if(character.equals(new String(character.getBytes("GB2312"), "GB2312"))) {
+        	//temp = new String(character.getBytes("GB2312"),"UTF-8");
+        	enCode = "GB2312" ;
+        }else if(character.equals(new String(character.getBytes("US-ASCII"), "US-ASCII"))) {
+        	//temp = new String(character.getBytes("US-ASCII"),"UTF-8");
+        	enCode = "US-ASCII" ;
+        }else if(character.equals(new String(character.getBytes("UTF-16BE"), "UTF-16BE"))) {
+        	//temp = new String(character.getBytes("UTF-16BE"),"UTF-8");
+        	enCode = "UTF-16BE" ;
+        }else if(character.equals(new String(character.getBytes("UTF-16LE"), "UTF-16LE"))) {
+        	//temp = new String(character.getBytes("UTF-16LE"),"UTF-8");
+        	enCode = "UTF-16LE" ;
+        }else if(character.equals(new String(character.getBytes("UTF-16"), "UTF-16"))) {
+        	//temp = new String(character.getBytes("UTF-16"),"UTF-8");
+        	enCode = "UTF-16" ;
+        }
+		//System.out.println("---------character---->> :" + temp);
+		//System.out.println("---------enCode---->> :" + enCode);
+		return enCode ;
+	}
+	
 	/**
 	 * 上传文件 到服务器  原始处理方法
 	 * @param isUpdate 是否已经上传
@@ -262,129 +390,5 @@ public class CalcUtil {
 			return fb;
 		}
 		
-	}
-	
-	/**
-	 * 处理拼接的字符串
-	 * @param parentId
-	 * @return
-	 */
-	public static String[] dealParentId(String parentId){
-		String[] arr = new String[2] ;
-		
-		String navAlise = null ;
-		//处理  父  子  级的 拼装
-		if (parentId == null || "".equals(parentId) || "0".equals(parentId)) {
-			parentId = "0";
-			navAlise = "";
-		} else {
-			String[] pgs = parentId.split("@");
-
-			parentId = pgs[0].trim();
-			parentId = parentId.replaceFirst("^0*", "");
-			if (pgs.length == 1) {
-				navAlise = "-";
-			} else {
-				String level = pgs[1];
-				if ("".equals(level)) {
-					navAlise = "";
-				} else {
-					navAlise = level + "-";
-				}
-			}
-		}
-		arr[0] = parentId ;
-		arr[1] = navAlise ;
-		return arr ;
-	}
-	
-	/**
-	 * wfluo 2015.07.23 add
-	 * 读取系统config.properties配置文件
-	 * @return
-	 */
-	public static Properties readConfigProperties() {
-		Properties props = new Properties();
-		try {
-			props.load(CalcUtil.class.getClassLoader().getResourceAsStream("config.properties"));
-		}catch (IOException e) {
-			e.printStackTrace();
-		}
-		return props ;
-	}
-	
-	public static void downloadFile(URL theURL,String fileName, String filePath,String encode)
-			throws IOException {
-		
-		File dirFile = new File(filePath);
-		if (!dirFile.exists()) {// 文件路径不存在时，自动创建目录
-			dirFile.mkdir();
-		}
-		// 从服务器上获取图片并保存
-		HttpURLConnection connection =(HttpURLConnection) theURL.openConnection();
-		//System.out.println("get Encode : " + connection.getContentEncoding());
-		connection.setRequestProperty("User-Agent","Mozilla/5.0 ( compatible ) ");
-		connection.setRequestProperty("Accept","*/*");
-		//connection.setRequestProperty("contentType", encode);
-		connection.setConnectTimeout(5 * 1000);  
-		connection.setRequestProperty("Accept-Language", "zh-CN");  
-		connection.setDoOutput(true);
-        connection.setDoInput(true);
-        connection.setInstanceFollowRedirects(false); 
-        connection.setRequestMethod("POST"); 
-        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded"); 
-        connection.setRequestProperty("charset", "utf-8");
-        connection.setUseCaches (false);
-
-		
-		
-		InputStream in = connection.getInputStream();
-		FileOutputStream os = new FileOutputStream(filePath + "/" + fileName);
-		byte[] buffer = new byte[4 * 1024];
-		int read;
-		while ((read = in.read(buffer)) > 0) {
-			os.write(buffer, 0, read);
-		}
-		os.close();
-		in.close();
-	}
-	
-	/**
-	 * 字符串编码格式判断及转码
-	 * @param character
-	 * @return
-	 * @throws UnsupportedEncodingException
-	 */
-	private static String judyCharacterEncoding(String character) throws UnsupportedEncodingException{
-		String temp = null ;
-		String enCode = null ;
-		if(character.equals(new String(character.getBytes("ISO-8859-1"), "ISO-8859-1"))) {
-          temp = new String(character.getBytes("ISO-8859-1"),"UTF-8");
-          enCode = "ISO-8859-1" ;
-        }else if(character.equals(new String(character.getBytes("GBK"), "GBK"))) {
-            temp = new String(character.getBytes("GBK"),"UTF-8");
-            enCode = "GBK" ;
-        }else if(character.equals(new String(character.getBytes("UTF-8"), "UTF-8"))) {
-        	temp = new String(character.getBytes("UTF-8"),"UTF-8");
-        	enCode = "UTF-8" ;
-        }else if(character.equals(new String(character.getBytes("GB2312"), "GB2312"))) {
-        	temp = new String(character.getBytes("GB2312"),"UTF-8");
-        	enCode = "GB2312" ;
-        }else if(character.equals(new String(character.getBytes("US-ASCII"), "US-ASCII"))) {
-        	temp = new String(character.getBytes("US-ASCII"),"UTF-8");
-        	enCode = "US-ASCII" ;
-        }else if(character.equals(new String(character.getBytes("UTF-16BE"), "UTF-16BE"))) {
-        	temp = new String(character.getBytes("UTF-16BE"),"UTF-8");
-        	enCode = "UTF-16BE" ;
-        }else if(character.equals(new String(character.getBytes("UTF-16LE"), "UTF-16LE"))) {
-        	temp = new String(character.getBytes("UTF-16LE"),"UTF-8");
-        	enCode = "UTF-16LE" ;
-        }else if(character.equals(new String(character.getBytes("UTF-16"), "UTF-16"))) {
-        	temp = new String(character.getBytes("UTF-16"),"UTF-8");
-        	enCode = "UTF-16" ;
-        }
-		System.out.println("---------character---->> :" + temp);
-		System.out.println("---------enCode---->> :" + enCode);
-		return enCode ;
 	}
 }
